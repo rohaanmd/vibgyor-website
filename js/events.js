1653c8f03a5aa05196763f5e8efe6bf1959f52ee
mohd rohaan <panzerxhack@gmail.com>
2023-04-01 19:52:42 +0530

    website complete

---
+++ js/events.js
@@ -0,0 +1,471 @@
+var reg_link = "#"
+
+var cultural_events = [
+    {   id:"cul_1",
+        logo: "./assets/icons/singing.png",
+        title: "Singing",
+        info:["Time Limit for Solo/group/duet Singing is max. 3 minutes with 5 students in group singing and Max. 2 persons can accompany the singer for instrumental support if required.",
+        "Accompanying persons are not eligible for certificates and have no need for registration. They must be students. Participants must bring all the instruments.",
+        "Choice of the song is open to the participants but the song should not have any slang or derogatory language.",
+        "The participants will be judged on the song selection, voice quality, clarity, rhythm, and appeal to the audience.",
+        "The decision of the Judges will be final and abiding.",],
+        reg_link
+    },
+    {   id:"cul_2",
+        logo: "./assets/icons/dance.png",
+        title: "Dance",
+        info:["There may be a time limit for the performance, usually ranging from 2-6 minutes (strictly). Make sure to stick to the allotted time, as going over may result in disqualification or point deductions. ",
+        "The music used should also be appropriate and free from offensive or vulgar lyrics. Any copyrighted music should be used with proper permissions. ",
+        "The costumes worn by the performers should be appropriate for the dance and not revealing or offensive. ",
+        "The judges may be looking for certain criteria such as technique, choreography, synchronization, creativity, and overall performance.",],
+        reg_link
+    },
+    {   id:"cul_3",
+        logo: "./assets/icons/fashion.png",
+        title: "Fashion",
+        info:["One entry per college.",
+        "It is a team event. A team can have 14 - 15 members.",
+        "The time limit for every team would be 15 minutes (Including setup and the performance both)",
+        "Negative marking if participants exceed the time limit and any kind of vulgarity in their props.",'Theme for fashion show “Bollywood “.',
+        "Green room will be provided for changing purposes.",
+        "Should carry their tracks in a pen drive.",
+        "Teams have to bring their own props.",
+        "The decision of the judges will be final and binding.",
+        "The use of cigarettes, alcohol, and any unfair means is strongly prohibited.",
+        "Teams will be judged on costumes, theme, walking stance, and attitude."
+    ],
+        reg_link
+    },
+    {   id:"cul_4",
+        logo: "./assets/icons/vibgyor.png",
+        title: "Mr. & Miss Vibgyor",
+        info:["You have to be in the formal.",
+        "Organisers are not responsible if there is any delay from your side.",
+        "Introduction not more than 40 to 50 sec.",
+        "Vulgarity and obscenity will not be entertained in any form.",
+        ],
+        reg_link
+    },
+    {   id:"cul_5",
+        logo: "./assets/icons/band.png",
+        title: "DJ/Band Battle",
+        info:["DJ or Band performance could not be more than 5 min.",
+        "You must manage your band according to your performance. We will not provide you with any management team.",
+        "We will not provide you with any specific instrument.",
+        "If there is any delay from your side, we are not responsible for that.",],
+        reg_link
+    },
+    {   id:"cul_6",
+        logo: "./assets/icons/stand.png",
+        title: "Standup Comedy",
+        info:["This event is for individual participant Limited entries based on a first come first serve basis would be there.",
+        "There is no specific theme for Standup.",
+        "The maximum time duration would be 4-5 minutes.",
+        "A negative Marking would be there for exceeding the time limit.",
+        "Participants will be judged on the basis of content,fluency, spontaneity, presentation, and sense of humor.",
+        ],
+        reg_link
+    },
+    {   id:"cul_7",
+        logo: "./assets/icons/poet.png",
+        title: "Kavi Sammelan",
+        info:["The Participation should be individual.",
+        "There should be no controversial or inappropriate content.",
+        "The time limit will be 5 minutes for every participant.",
+        "Participants should not exceed the given time limit, there will be negative marks for that.",
+        ],
+        reg_link
+    },
+    {   id:"cul_8",
+        logo: "./assets/icons/skit.png",
+        title: "Skit",
+        info:["Participants need to report 30 minutes before the given time.",
+        "Late entries will not be allowed to participate or play their skit.",
+        "A maximum of 12 participants are allowed in a group.",
+        "Controversial and vulnerable topics will not be tolerated.","No registration is allowed at the time of the event.",
+        ],
+        reg_link
+    },
+    {   id:"cul_9",
+        logo: "./assets/icons/natak.png",
+        title: "Nukkad Natak",
+        info:["NUKKAD NATAK should highlight a relevant social concern.",
+        "Negative marking for exceeding the time limit.",
+        "No negative image should be portrayed toward anyone.",
+        ],
+        reg_link
+    },
+    {   id:"cul_10",
+        logo: "./assets/icons/video.png",
+        title: "Short Flim Making",
+        info:["Duration of the movie will be-2-5 minutes",
+        "Students have to bring their own equipment (camera, stand, etc)",
+        "Participants will do their own editing.",
+        "You have to post this short film on your social media accounts with these hashtags (#IMSEC, #vibgyor2023inimsec, #IMSECMBA)",
+        "Do not use clips from previous work that are available in the public domain.",
+        ],
+        reg_link
+    },
+    {   id:"cul_11",
+        logo: "./assets/icons/collage.png",
+        title: "Collage Making",
+        info:["It is a team event of 1-hour duration.",
+        "Each team consists of 2 members, each from the same institute",
+        "Old magazines, newspapers, posters, etc will be provided to each team",
+        "team have to bring color, highlighter and sketch pen, scissor, gum for themselves",
+        "the theme will be given on the spot",
+    
+        ],
+        reg_link
+    },
+    {   id:"cul_12",
+        logo: "./assets/icons/handi.png",
+        title: "Handicraft",
+        info:["Theme: Just roll with it.",
+        " The competition is open to all college students ",
+        "Each participant can submit only one entry per category.",
+        "Entries will be judged based on creativity, originality, use of materials and overall presentation.",
+        "Selected works will be showcased/exhibited in B-Block starting at 10:30 am on the 7th.",
+        "Any entry that includes obvious unethical material or content will be immediately disqualified from the competition.",
+        "",
+        ],
+        reg_link
+    },
+    {   id:"cul_13",
+        logo: "./assets/icons/kavi.png",
+        title: "Poetry",
+        info:["You have to finish your poetry within 3 to 4 min.",
+        "If we found that you are using basilect words in your poetry, we will disqualify you.",
+        "Poetry must be written by yourself.",
+        "Contestants are allowed to read from the poem’s text, but bonus marks will be given to those that can memorize the poem.",
+        ],
+        reg_link
+    },
+    {   id:"cul_14",
+        logo: "./assets/icons/mimicry.png",
+        title: "Mimicry",
+        info:["This is a solo event.",
+        "Each participant shall be given 5 minutes",
+        "Judgement will be based on imitating skill, variety of sounds and voices imitated and overall presentation.",
+        "No vulgar or unparliamentary language will be entertainment.",
+        "The decision of the judge will be final",
+        ],
+        reg_link
+    },
+    {   id:"cul_15",
+        logo: "./assets/icons/poster.png",
+        title: "Poster Making",
+        info:["Maximum team size is 2.",
+        "The subject of the poster will be given by the organizer on the spot.",
+        "Duration will not be more than 90 mins.",
+        "Poster should be in A3 size sheet.",
+        "Candidates shall bring their own material like, A3 Sheets, brushes, paints etc.",
+        ],
+        reg_link
+    },
+    {   id:"cul_16",
+        logo: "./assets/icons/rangoli.png",
+        title: "Rangoli",
+        info:["Maximum team size is 3.",
+        "Duration will not be more than 1 hours 30 minutes.",
+        "Participants shall bring their material.",
+        "The medium and form for expression can be free hand, pictorial and descriptive",
+        "The participants shall have to prepare a Rangoli within the space provided by the organizers.",
+        ],
+        reg_link
+    },
+    {   id:"cul_17",
+        logo: "./assets/icons/face.png",
+        title: "Face Painting",
+        info:["Only 2 participants are allowed in face painting.",
+        "Duration will not be more than 90 mins.",
+        "Participants shall bring their materials.",
+        "If we found any stencil on the face before the event you will be disqualified.",
+        ],
+        reg_link
+    },
+    {   id:"cul_18",
+        logo: "./assets/icons/mehendi.png",
+        title: "Mehendi",
+        info:["Only 2 participants are allowed.",
+        "Duration will not be more than 2 hours.",
+        "Participants shall bring their materials.",
+        "If we found any stencil on the hand before the event you will be disqualified.",
+        "You have to make this mehndi by your hand, not with any type of stencil tools",
+        ],
+        reg_link
+    },
+    {   id:"cul_19",
+        logo: "./assets/icons/tattoo.png",
+        title: "Tattoo Making",
+        info:["Team event (2 members – one who will draw and the other person on whom art will be performed).",
+        "Time – 1 hour",
+        "Materials will not be provided",
+        "Accompanying persons are not eligible for certificates and no need for registration.",
+        "The participants will be judged on basis of theme selection, clarity, creativity, presentation, and completion on time.",
+        "No mobile or Internet means would be allowed to use at the time of the Competition.",
+        "The decision of the Judges will be final and abiding.",
+        ],
+        reg_link
+    },
+    {   id:"cul_20",
+        logo: "./assets/icons/sketch.png",
+        title: "Sketching",
+        info:["The topic will be given on the spot",
+        "Time – 1 hour",
+        "Materials will not be provided",
+        "Participants have to carry their own materials",
+        "The participants will be judged on basis of theme selection, clarity, creativity, presentation, and completion on time.",
+        "No mobile or Internet means would be allowed to use at the time of the Competition.",
+        "The decision of the Judges will be final and abiding.",
+        ],
+        reg_link
+    },
+    {   id:"cul_21",
+        logo: "./assets/icons/waste.png",
+        title: "Best Out Of Waste",
+        info:["Participants will be given 1 hour and 30 min. to show their creativity and 2 minutes to talk about the same.",
+        "The required waste material and stationery like scissors, thread, etc. should be brought by the participants.",
+        "Waste material could be anything like tetra packs, bottles, newspapers, old wires, old utensils, jute material or any second-hand items that otherwise would be thrown away.",
+        "The material would be rejected if not found to be a waste product or a second-hand item.",
+        "No ready or semi-finished model or matter would be accepted by participants in the competition.",
+        "The decision of the Judges will be final and abiding.",
+        ],
+        reg_link
+    },
+    {   id:"cul_22",
+        logo: "./assets/icons/hunt.png",
+        title: "Tresure Hunt",
+        info:["The event is open to all currently enrolled students of the college.",
+        "Each team must consist of a minimum of 3 and a maximum of 5 members.",
+        "all teams must complete the tasks and reach the endpoint within that time.",
+        "The treasure will be hidden at a location that can only be reached by solving all the clues. The team that finds the treasure first will be declared the winner.",
+        "In case of any disputes, the decision of the organizers will be final.",
+        "Any form of cheating, misconduct, or unsportsmanlike behavior will result in immediate disqualification.",
+        "Clues will be provided to the teams at various points throughout the event. Teams must solve these clues to move on to the next task.",
+        "There will be certain areas in the college that are off-limits for the event. Participants must not enter these areas, and doing so will lead to disqualification.",
+        ],
+        reg_link
+    },
+    {   id:"cul_23",
+        logo: "./assets/icons/story.png",
+        title: "Story Writing",
+        info:[
+            "",
+            "",
+            "",
+            "",
+            "",
+        ],
+        reg_link
+    },
+    {   id:"cul_24",
+        logo: "./assets/icons/travel.png",
+        title: "Creative Travel Stories",
+        info:[
+            "",
+            "",
+            "",
+            "",
+            "",
+        ],
+        reg_link
+    },
+
+
+
+]
+
+var tech_events = [    
+    {   id:"tech_1",
+logo: "./assets/icons/blind.png",
+title: "Blind Coding",
+info:["Each participant has to write the code for the given problem with monitor off.",
+" Participants those codes get executed will be given higher preference.",
+"The 2nd preference will be given to code with the least number of errors.",
+],
+reg_link
+},
+    
+    {   id:"tech_2",
+logo: "./assets/icons/build.png",
+title: "Build & win",
+info:[
+    "The students will make basic elcetronic/Digital circuit using minimum components and give a short presentation on it",
+],
+reg_link
+},
+    {   id:"tech_3",
+logo: "./assets/icons/war.png",
+title: "Code War",
+info:[
+    "Maximum team size will be two.",
+    "All teams will be given the same set of problems to solve.",
+],
+reg_link
+},
+    {   id:"tech_4",
+logo: "./assets/icons/codecode.png",
+title: "CoDecode",
+info:[
+    "Participants can code in their suitable coding language.",
+    "There will be 2 rounds. ",
+    "Round 1: Guess the error/code. Round 2: Code it",
+],
+reg_link
+},
+    {   id:"tech_5",
+logo: "./assets/icons/apti.png",
+title: "Aptifesta",
+info:[
+    " Participants have to register in group of two.",
+    "The test will be conducted in 2 rounds of 30 min. each.",
+],
+reg_link
+},
+    {   id:"tech_6",
+logo: "./assets/icons/logo.png",
+title: "Logo Designing Competition",
+info:[
+    "The students will carry their own laptop with the required software for logo designing and it should be fully charged.",
+    "Logo should be free from plagiarism or copyright voilation of any kind.",
+    " Logo artwork should be atleast 2 inches X 2 inches.",
+    "Logo should be design using colors (Not Grayscale or Black & White)",
+],
+reg_link
+},
+    {   id:"tech_7",
+logo: "./assets/icons/bgmi.png",
+title: "BGMI Competition",
+info:[
+    "Maximum team size will be 4. ",
+    "Bring your own fully charged Laptop/Mobile with internet connection. ",
+    "Basic Knowledge of BGMI.",
+],
+reg_link
+},
+    {   id:"tech_8",
+logo: "./assets/icons/robo.png",
+title: "ROBO-War",
+info:[
+    "Each Team has to bring their own robot and can have max. 3 members.",
+    "Structure of robot should not be changed during competition.",
+    "Robot must complete the race in max 5 minutes. ",
+    "There will be a single round of competiotion (No Semifinal/Final round)",
+],
+reg_link
+},
+    {   id:"tech_9",
+logo: "./assets/icons/shark.png",
+title: "Shark Tank",
+info:[
+    "Each Team can have maximum 3 members. ",
+    "Business Idea should be Novel and feasible.",
+    "Each Team will be given 5-7 minutes to pitch their business model.",
+],
+reg_link
+},
+    {   id:"tech_10",
+logo: "./assets/icons/project.png",
+title: "Innovative Project Demonstration",
+info:[
+    "The students will be given 10-12 minutes for presentation on any emerging technologies with 2 minutes inclusive for questioniare.",
+    "The Participants should carry their own laptops. ",
+    "Evaluation will be done on Way of presenting, articulation of speech, choice of topic and answering to questions",
+],
+reg_link
+},
+    {   id:"tech_11",
+logo: "./assets/icons/video.png",
+title: "Best Videography of the event",
+info:[
+    "Arrive early to familiarize yourself with the location and layout of the event space. ",
+    "quality of the video should be above 1080p.",
+    "Use a tripod or stabilizer to keep your camera steady and avoid shaky footage.",
+    "Make sure there is adequate lighting in the venue, and consider bringing additional lighting if necessary.",
+],
+reg_link
+},
+
+]
+
+
+var x = document.getElementById("event-container");
+x.innerHTML = cultural_events.map(e=>`<div class="box">
+<div class="img_container">
+    <img
+        src="${e.logo}"
+        alt="${e.title}">
+</div>
+<div class="content_container">
+    <h5>${e.title}</h5>
+    <div class="button_container">
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#${e.id}">More</button>
+        <a href="#" type="button" class="btn
+            ">Register</a>
+    </div>
+</div>
+</div>`).join('');
+
+var y = document.getElementById("modal-container");
+y.innerHTML =cultural_events.map(e=>`<div class="modal fade" id="${e.id}" tabindex="-1" aria-labelledby="${e.title}" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered modal-lg">
+  <div class="modal-content  cus-modal">
+    <div class="modal-header">
+      <h1 class="modal-title modal_cus_title" id="exampleModalLabel">${e.title}</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <ol class="modal_cus_list"> 
+    ${e?.info?.map(r=>`<li>${r}</li>`).join('')}
+    </ol>
+     
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn modal_cus_btn" data-bs-dismiss="modal">Register</button>
+     
+    </div>
+  </div>
+</div>
+</div>`).join('');
+
+
+var v = document.getElementById("event-container-tech");
+v.innerHTML = tech_events.map(e=>`<div class="box">
+<div class="img_container">
+    <img
+        src="${e.logo}"
+        alt="${e.title}">
+</div>
+<div class="content_container">
+    <h5>${e.title}</h5>
+    <div class="button_container">
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#${e.id}">More</button>
+        <a href="#" type="button" class="btn
+            ">Register</a>
+    </div>
+</div>
+</div>`).join('');
+
+var w = document.getElementById("modal-container-tech");
+w.innerHTML =tech_events.map(e=>`<div class="modal fade" id="${e.id}" tabindex="-1" aria-labelledby="${e.title}" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered modal-lg ">
+  <div class="modal-content cus-modal">
+    <div class="modal-header">
+      <h1 class="modal-title modal_cus_title" id="exampleModalLabel">${e.title}</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <ol class="modal_cus_list"> 
+    ${e?.info?.map(r=>`<li>${r}</li>`).join('')}
+    </ol>
+     
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn modal_cus_btn" data-bs-dismiss="modal">Register</button>
+     
+    </div>
+  </div>
+</div>
+</div>`).join('');
+
